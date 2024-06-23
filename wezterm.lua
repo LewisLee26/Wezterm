a20ab10bfa07a09d62362eba797cd50523fd47e5
LewisLee26 <lewis.patrick.lee@gmail.com>
2024-06-24 00:32:59 +0100

    Initial commit

---
+++ wezterm.lua
@@ -0,0 +1,315 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- Colorscheme
+config.colors = {
+  foreground = "#f0f3f6",
+  background = "#0a0c10",
+
+  cursor_bg = "#f0f3f6",
+  cursor_border = "#f0f3f6",
+  cursor_fg = "#0a0c10",
+
+  selection_bg = "#204670",
+  selection_fg = "#f0f3f6",
+
+  scrollbar_thumb = "#222222",
+  split = "#444444",
+
+  ansi = {
+    "#0a0c10", -- "7a828e", change this so that curse doesn't make bg grey
+    "#ff9492",
+    "#26cd4d",
+    "#f0b72f",
+    "#3137ff", -- this has been changed from the original theme to be darker
+    "#cb9eff",
+    "#39c5cf",
+    "#d9dee3",
+  },
+  brights = {
+    "#9ea7b3",
+    "#ffb1af",
+    "#4ae168",
+    "#f7c843",
+    "#91cbff",
+    "#dbb7ff",
+    "#56d4dd",
+    "#ffffff",
+  },
+}
+
+config.leader = { key = "Space", mods = "CTRL", timeout_milliseconds = 1000 }
+config.default_prog = { "wsl" }
+config.font_size = 11.0
+config.enable_kitty_graphics = false
+config.hide_tab_bar_if_only_one_tab = false
+config.use_fancy_tab_bar = false
+config.show_new_tab_button_in_tab_bar = false
+config.status_update_interval = 1000
+config.tab_max_width = 60
+config.tab_bar_at_bottom = false
+
+-- Panes
+config.inactive_pane_hsb = {
+  saturation = 0.8,
+  brightness = 0.6,
+}
+
+wezterm.on("update-right-status", function(window, pane)
+  local workspace_or_leader = window:active_workspace()
+  -- Change the worspace name status if leader is active
+  if window:active_key_table() then
+    workspace_or_leader = window:active_key_table()
+  end
+  if window:leader_is_active() then
+    workspace_or_leader = "LEADER"
+  end
+
+  local time = wezterm.strftime("%H:%M")
+  local battery = ""
+  for _, b in ipairs(wezterm.battery_info()) do
+    battery = string.format("%.0f%%", b.state_of_charge * 100)
+  end
+
+  window:set_right_status(wezterm.format({
+    { Foreground = { Color = "FFB86C" } },
+    "ResetAttributes",
+    { Text = wezterm.nerdfonts.oct_table .. " " .. workspace_or_leader },
+    { Text = " | " },
+    { Text = " " .. battery .. " " },
+    { Text = " | " },
+    { Text = wezterm.nerdfonts.md_clock .. " " .. time },
+    { Text = " | " },
+  }))
+end)
+
+-- Function to check if the pane is running Neovim
+local function is_vim(pane)
+  return pane:get_user_vars().IS_NVIM == "true"
+end
+
+-- Direction keys mapping
+local direction_keys = {
+  Left = "h",
+  Down = "j",
+  Up = "k",
+  Right = "l",
+  h = "Left",
+  j = "Down",
+  k = "Up",
+  l = "Right",
+}
+
+-- Function to create navigation and resize actions
+local function split_nav(resize_or_move, key)
+  return {
+    key = key,
+    mods = resize_or_move == "resize" and "META" or "CTRL",
+    action = wezterm.action_callback(function(win, pane)
+      if is_vim(pane) then
+        win:perform_action({
+          SendKey = { key = key, mods = resize_or_move == "resize" and "META" or "CTRL" },
+        }, pane)
+      else
+        if resize_or_move == "resize" then
+          win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
+        else
+          win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
+        end
+      end
+    end),
+  }
+end
+
+-- Define keybindings
+config.keys = {
+  -- Splitting panes
+  {
+    mods = "LEADER",
+    key = "-",
+    action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+  },
+  {
+    mods = "LEADER",
+    key = "=",
+    action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+  },
+  -- Maximizing a pane
+  {
+    mods = "LEADER",
+    key = "m",
+    action = wezterm.action.TogglePaneZoomState,
+  },
+  -- Rotating panes
+  {
+    mods = "LEADER",
+    key = "Space",
+    action = wezterm.action.RotatePanes("Clockwise"),
+  },
+  -- Swapping panes
+  {
+    mods = "LEADER",
+    key = "0",
+    action = wezterm.action.PaneSelect({
+      mode = "SwapWithActive",
+    }),
+  },
+  -- Activate copy mode
+  {
+    key = "Enter",
+    mods = "LEADER",
+    action = wezterm.action.ActivateCopyMode,
+  },
+  {
+    key = "r",
+    mods = "LEADER",
+    action = wezterm.action.ActivateKeyTable({ name = "resize_pane", one_shot = false }),
+  },
+
+  -- Move between split panes
+  split_nav("move", "h"),
+  split_nav("move", "j"),
+  split_nav("move", "k"),
+  split_nav("move", "l"),
+  -- Resize panes
+  split_nav("resize", "h"),
+  split_nav("resize", "j"),
+  split_nav("resize", "k"),
+  split_nav("resize", "l"),
+  -- Existing keybindings
+  {
+    key = "f",
+    mods = "LEADER",
+    action = wezterm.action.ToggleFullScreen,
+  },
+  {
+    key = "P",
+    mods = "SHIFT|CTRL",
+    action = wezterm.action.ActivateCommandPalette,
+  },
+  {
+    key = "x",
+    mods = "LEADER",
+    action = wezterm.action.CloseCurrentPane({ confirm = false }),
+  },
+  {
+    key = "X",
+    mods = "LEADER",
+    action = wezterm.action.CloseCurrentTab({ confirm = false }),
+  },
+  {
+    key = "m",
+    mods = "LEADER",
+    action = wezterm.action.ActivateKeyTable({ name = "move_tab", one_shot = false }),
+  },
+  {
+    key = "t",
+    mods = "LEADER",
+    action = wezterm.action.ShowTabNavigator,
+  },
+  {
+    key = "n",
+    mods = "LEADER",
+    action = wezterm.action.SpawnTab("CurrentPaneDomain"),
+  },
+  { mods = "LEADER", key = "h", action = wezterm.action.ActivateTabRelative(-1) },
+  { mods = "LEADER", key = "l", action = wezterm.action.ActivateTabRelative(1) },
+  -- Rename tab
+  {
+    key = ",",
+    mods = "LEADER",
+    action = wezterm.action.PromptInputLine({
+      description = "Enter new tab title",
+      action = wezterm.action_callback(function(window, _, line)
+        if line then
+          window:perform_action(wezterm.action.SetTabTitle(line), window:active_pane())
+        end
+      end),
+    }),
+  },
+  -- Workspace
+  {
+    key = "w",
+    mods = "LEADER",
+    action = wezterm.action.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }),
+  },
+  {
+    key = "W",
+    mods = "LEADER",
+    action = wezterm.action.PromptInputLine({
+      description = wezterm.format({
+        { Attribute = { Intensity = "Bold" } },
+        { Foreground = { AnsiColor = "Fuchsia" } },
+        { Text = "Enter name for new workspace" },
+      }),
+      action = wezterm.action_callback(function(window, pane, line)
+        if line then
+          window:perform_action(
+            wezterm.action.SwitchToWorkspace({
+              name = line,
+            }),
+            pane
+          )
+        end
+      end),
+    }),
+  },
+  {
+    key = "[",
+    mods = "LEADER",
+    action = wezterm.action.SwitchWorkspaceRelative(1),
+  },
+  {
+    key = "]",
+    mods = "LEADER",
+    action = wezterm.action.SwitchWorkspaceRelative(-1),
+  },
+  {
+    key = "Q",
+    mods = "LEADER",
+    action = wezterm.action.QuitApplication,
+  },
+  {
+    key = "z",
+    mods = "LEADER",
+    action = wezterm.action.TogglePaneZoomState,
+  },
+}
+
+-- Quick tab movement
+for i = 1, 9 do
+  table.insert(config.keys, {
+    key = tostring(i),
+    mods = "LEADER",
+    action = wezterm.action.ActivateTab(i - 1),
+  })
+end
+
+config.key_tables = {
+  resize_pane = {
+    { key = "h", action = wezterm.action.AdjustPaneSize({ "Left", 1 }) },
+    { key = "j", action = wezterm.action.AdjustPaneSize({ "Down", 1 }) },
+    { key = "k", action = wezterm.action.AdjustPaneSize({ "Up", 1 }) },
+    { key = "l", action = wezterm.action.AdjustPaneSize({ "Right", 1 }) },
+    { key = "Escape", action = "PopKeyTable" },
+    { key = "Enter", action = "PopKeyTable" },
+  },
+  move_tab = {
+    { key = "h", action = wezterm.action.MoveTabRelative(-1) },
+    { key = "j", action = wezterm.action.MoveTabRelative(-1) },
+    { key = "k", action = wezterm.action.MoveTabRelative(1) },
+    { key = "l", action = wezterm.action.MoveTabRelative(1) },
+    { key = "Escape", action = "PopKeyTable" },
+    { key = "Enter", action = "PopKeyTable" },
+  },
+}
+
+-- Return the configuration to wezterm
+return config
